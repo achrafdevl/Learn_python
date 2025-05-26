5f2b3a26ef5c7b95c31f3b9dfca9b3fb79271cd1
achraf <achrafchair6@gmail.com>
2025-05-26 12:53:55 +0100

    pass exam questions

---
+++ examen.py
@@ -0,0 +1,126 @@
+# a = 10
+# while a < 15:
+#     print(a)
+# print("fini")
+
+
+# a = 10
+# print(a > 10)
+# if a <= 10:
+#     a = a + 5
+# else:
+#     a = a - 5
+# if a > 10:
+#     print("Supérieur à 10")
+# else:
+#     print("Inférieur ou égal à 10")
+# if a == 10:
+#     print("Egal à 10")
+# if a == 15:
+#     print("Egal à 15")
+# if a == 5:
+#     print("Egal à 5")
+# else:
+#     print("Différent de 5")
+
+
+
+# for i in range(0, 2):
+#     print(i)
+
+
+# def f1(a):
+#     return "a"
+ 
+# def f2(a):
+#     return a+a
+ 
+# def f3(a):
+#     print(a)
+ 
+# print(f1("b") + str(f2(5)))
+# f3(f2("1"))*
+
+
+# def ma_fonction():
+#     b = 5
+ 
+# ma_fonction()
+# ma_fonction()
+# ma_fonction()
+# print(b)
+
+# a = 5
+ 
+# def secret(b):
+#     print(b+b)
+ 
+# print(a)
+# secret("a")
+# secret(a)
+
+
+#! Q°9
+# print("Quelle est la capitale de l'Italie ?")
+# print("A - Rome")
+# print("B - Venise")
+# print("C - Pise")
+# choix = input("Votre choix: ")
+# if choix == "A":
+#     print("Bonne réponse")
+# if choix == "B" or choix == "C":
+#     print("Mauvaise réponse")
+# else:
+#     print("Vous devez rentrer A, B ou C")
+
+
+#! Q°10
+
+# print("Quelle est la capitale de l'Italie ?")
+# print("A - Rome")
+# print("B - Venise")
+# print("C - Pise")
+# choix = input("Votre choix: ")
+# if choix == "A":
+#     print("Bonne réponse")
+# elif choix == "B" or choix == "C":
+#     print("Mauvaise réponse")
+# else:
+#     print("Vous devez rentrer A, B ou C")
+
+#! Q°16
+# a = 10
+# while a < 15:
+#     print(a)
+#     a = a + 2
+# print("fini")
+
+#! Q°17
+
+
+# a = input("Rentrez le nombre : ")
+# try:
+#     a_num = int(a)
+#     print(a)
+# except:
+#     print("ERREUR")
+# print("TOUT EST BON")
+
+#! Q°18
+# parcours_km = 53
+# print("Il vous reste " + parcours_km + "km à parcourir")
+
+#! Q°19
+# n = input("Donnez moi un nombre entre 1 et 10")
+
+# print("Votre nombre multiplié par 2 est : " + str(2* int(n)))
+
+#! Q°20
+
+# a = "Je"
+
+# b = " suis un "
+
+# c = "développeur"
+
+# print(a+b+c)
